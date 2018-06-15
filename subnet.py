bbfb5566deba99c8c2fe90c9ec3dd1a515118990
karthiksjsu <appikarthik@gmail.com>
2018-06-15 13:14:00 -0700

    added comments

--- subnet.py
+++ subnet.py
@@ -19,7 +19,8 @@ def subnet_calc():
 		while True:
 
 			ip_address=raw_input("Enter an IP address:")
-
+                        
+                        #Regex to Validate IP address
 			matched=re.match(r'^(2[0-1][0-9]|1[0-13-9][0-9]|22[0-3]|[0-9]{0,2}|12[0-68-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[0-9]?[0-9])$',ip_address)
 
 			if(matched):
@@ -30,10 +31,12 @@ def subnet_calc():
 
 
 
-		#Check for Subnet Mask Validity
+		#Check for SubnetMask Validity
 		while True:
 			
 			subnet_mask=raw_input("Enter a subnet mask:")
+
+			#Regex to Validate Subnetmask
 			matched=re.match(r'^(25[5|4|2]|24[8|0]|224|192|128|0])\.(25[5|4|2]|24[8|0]|224|192|128|0)\.(25[5|4|2]|24[8|0]|224|192|128|0)\.(25[5|4|2]|24[8|0]|224|192|128|0)$',subnet_mask)
                       
 			if (matched): 
